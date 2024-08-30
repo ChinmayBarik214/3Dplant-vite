74fd35794967545c0882cabfaf427b648c328e1b
Chinmay <chinmaybarik214@gmail.com>
2024-08-30 20:24:31 +0530

    import plant model and adjust camera position

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { Vector3, HemisphericLight, MeshBuilder, ArcRotateCamera, SceneLoader } from '@babylonjs/core';
 import SceneComponent from 'babylonjs-hook';
+import "@babylonjs/loaders/glTF";
 import './App.css'
 
 const onSceneReady = scene => {
   const canvas = scene.getEngine().getRenderingCanvas();
   const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new Vector3(0, 0, 0), scene);
   camera.attachControl(canvas, true);
-
+  camera.position = new Vector3(100, 100, 0);
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
-  const box = MeshBuilder.CreateBox("box", {}, scene);
-  SceneLoader.ImportMeshAsync(["ground", "semi_house"], "https://assets.babylonjs.com/meshes/", "both_houses_scene.babylon")
-    .then((result) => {
-      result.meshes[0].position.y = -0.5;
-      const house = scene.getMeshByName("semi_house");
-      house.position.y = -0.5;
-      house.position.x = 2;
-      house.rotation.y = Math.PI / 2;
-    });
+  // const box = MeshBuilder.CreateBox("box", {}, scene);
+  SceneLoader.ImportMesh("", "https://raw.githubusercontent.com/ChinmayBarik214/MeshesLibrary/main/", "plant.glb", scene, function (newMeshes) {
+    camera.target = newMeshes[0];
+    newMeshes[0].scaling = new Vector3(0.5, 0.5, 0.5);
+  });
 }
 
 // Will run on every frame render.

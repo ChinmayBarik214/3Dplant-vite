36e295ae5fce6034d6922c06e63f3d76c11754a6
Chinmay <chinmaybarik214@gmail.com>
2024-08-30 08:09:09 +0530

    manipulate multiple imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Vector3, HemisphericLight, MeshBuilder, ArcRotateCamera } from '@babylonjs/core';
+import { Vector3, HemisphericLight, MeshBuilder, ArcRotateCamera, SceneLoader } from '@babylonjs/core';
 import SceneComponent from 'babylonjs-hook';
 import './App.css'
 
@@ -10,6 +10,14 @@ const onSceneReady = scene => {
   const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
   const box = MeshBuilder.CreateBox("box", {}, scene);
+  SceneLoader.ImportMeshAsync(["ground", "semi_house"], "https://assets.babylonjs.com/meshes/", "both_houses_scene.babylon")
+    .then((result) => {
+      result.meshes[0].position.y = -0.5;
+      const house = scene.getMeshByName("semi_house");
+      house.position.y = -0.5;
+      house.position.x = 2;
+      house.rotation.y = Math.PI / 2;
+    });
 }
 
 // Will run on every frame render.
